9e196ea7f1342a00621ad1c4e6f316384d5e55fc
Jason Novinger <jnovinger@gmail.com>
2015-12-14 23:44:35 -0600

    Working client and server side trackers
    - Provides tracking/sending of impression, detail views, adds to cart,
      removes from cart, checkout steps, and transaction.

--- eecommerce/static/js/eecommerce.js
+++ eecommerce/static/js/eecommerce.js
@@ -6,13 +6,23 @@ var EETracker = (function(){
    * @param {array<object>} data
    */
   var EETracker = function(data) {
+    this.init = this.init.bind(this);
+    this.initGA = this.initGA.bind(this);
+    this.ga = this.ga.bind(this);
     this.setData = this.setData.bind(this);
-    this.pushData = this.pushData.bind(this);
+    this.sendImpressions = this.sendImpressions.bind(this);
     this.getImpressions = this.getImpressions.bind(this);
     this.getImpression = this.getImpression.bind(this);
+    this.sendProductDetail = this.sendProductDetail.bind(this);
+    this.getProduct = this.getProduct.bind(this);
+
+    this.checkout = this.checkout.bind(this);
+    this.getCheckoutData = this.getCheckoutData.bind(this);
 
     this.init(data);
   };
+
+  var noop = function() {};
 
   /**
    * The data provided to the constructor for this page
@@ -30,7 +40,13 @@ var EETracker = (function(){
     if (typeof(data) == 'string') {
       data = JSON.parse(data);
     }
-    data.objects = data.objects || [];
+
+    data.items = data.items || {};
+    data.details = data.details || [];
+    data.impressions = data.impressions || [];
+    data.checkout = data.checkout || {};
+    data.page_type = data.page_type || 'list';
+
     this.data = data;
     return data;
   };
@@ -40,6 +56,10 @@ var EETracker = (function(){
    * @type {array}
    */
   EETracker.prototype.dataLayer = null;
+  EETracker.prototype.ga = null;
+  EETracker.prototype.debug = function() {
+    console.log(arguments);
+  };
 
   /**
    *
@@ -52,25 +72,50 @@ var EETracker = (function(){
    */
   EETracker.prototype.init = function(data) {
     data = this.setData(data);
+    data.gaID = 'UA-38721533-2';
+    this.type = data.type;
+    if (!data.debug) {
+      this.debug = noop;
+    }
     this.currencyCode = data.currency || this.currencyCode;
-    this.dataLayer = window[data.dataLayer] || window.dataLayer;
+    this.initGA();
     this.handleInitialActions();
   };
 
-  /**
-   * Actions that should be pushed to GA immediately
-   */
-  var initialActions = [
-    'getImpressions'
-  ];
+  EETracker.prototype.initGA = function() {
+    this.ga('create', this.data.gaID, 'none', 'ee');
+    this.ga('ee.require', 'ec');
+  };
+
+  EETracker.prototype.ga = function() {
+    this.debug.apply(this, arguments);
+    window.ga.apply(window.ga, arguments);
+  };
 
   /**
    *
    */
   EETracker.prototype.handleInitialActions = function() {
-    initialActions.forEach(function(element, index, array) {
-      this.pushData(this[element]());
-    }, this);
+    this.debug('start handleInitialActions');
+
+    // always send impressions immediately
+    this.sendImpressions();
+    switch (this.type) {
+      case 'detail':
+        this.sendProductDetail();
+        break;
+      case 'checkout':
+        this.sendImmediateCheckout();
+        break;
+      case 'success':
+        this.sendImmediateCheckout();
+        this.sendTransaction();
+        break;
+      default:
+        break;
+    };
+    this.ga('ee.send', 'pageview');
+    this.debug('end handleInitialActions');
   };
 
   /**
@@ -78,7 +123,16 @@ var EETracker = (function(){
    * @returns {array}
    */
   EETracker.prototype.getImpressions = function() {
-    return new impressionFieldObject(this.data.objects, this.currencyCode);
+    var data = this.data
+    var impressionHashes = data.impressions;
+
+    impressionHashes = impressionHashes.filter(function(item) {
+      return !this.data.details.includes(item);
+    }, this);
+
+    return impressionHashes.map(function(hash) {
+      return this.getImpression(this.data.items[hash]);
+    }, this);
   };
 
   /**
@@ -91,14 +145,182 @@ var EETracker = (function(){
   };
 
   /**
-   * Push data to the GA Enhanced Analytics queue
-   * @param {object} data
+   * Send impression data to GA
    */
-  EETracker.prototype.pushData = function(data) {
-    console.log("Logging: ");
-    console.log(data);
-    this.dataLayer.push(data);
+  EETracker.prototype.sendImpressions = function() {
+    var impressions = this.getImpressions();
+    impressions.forEach(function(impression) {
+      this.ga('ee.ec:addImpression', impression);
+    }, this);
+  };
+
+  /**
+   * Returns properly formatted `product` objects consumable by GA
+   * @param {object} item
+   * @returns {object}
+   */
+  EETracker.prototype.getProduct = function(productData) {
+    return new productObject(productData);
+  };
+
+  /**
+   *
+   */
+  EETracker.prototype.getProducts = function() {
+    return this.data.details.map(function(product) {
+      return this.getProduct(this.data.items[product]);
+    }, this);
+  }
+
+  /**
+   *
+   */
+  EETracker.prototype.getProductByHash = function(hash) {
+    var product = this.data.items[hash];
+    if (product) {
+      return this.getProducts(product);
+    }
+
+    return;
+  };
+
+  /**
+   * Send product data to GA
+   */
+  EETracker.prototype.sendProductDetail = function() {
+    var products = this.getProducts();
+    products.forEach(function(product) {
+      this.ga('ee.ec:addProduct', product);
+    }, this);
+  };
+
+  /**
+   *
+   */
+  EETracker.prototype.addToCart = function(hash, callback) {
+    var product = this.getProductByHash(hash);
+    if (!product) {
+      return;
+    }
+
+    this.ga('ee.ec:addProduct', product);
+    this.ga('ee.ec:setAction', 'add');
+    this.ga('ee.send', 'event', 'UX', 'click', 'add to cart', {
+      hitCallback: callback
+    });
+  }
+
+  /**
+   *
+   */
+  EETracker.prototype.removeFromCart = function(hash, callback) {
+    var product = this.getProductByHash(hash);
+    if (!product) {
+      return;
+    }
+
+    this.ga('ee.ec:addProduct', product);
+    this.ga('ee.ec:setAction', 'remove');
+    this.ga('ee.send', 'event', 'UX', 'click', 'remove from cart', {
+      hitCallback: callback
+    });
+  }
+
+  /**
+   *
+   */
+  EETracker.prototype.checkout = function(option) {
+    this.sendProductDetail();
+    this.ga('ee.ec:setAction', 'checkout', this.getCheckoutData(option));
+    this.ga('ee.send', 'pageview');
+  };
+
+  /**
+   *
+   */
+  EETracker.prototype.sendImmediateCheckout = function() {
+    if (this.data.checkout.immediate) {
+      this.checkout();
+    }
+  }
+
+  /**
+   *
+   */
+  EETracker.prototype.getCheckoutData = function(option) {
+    var data = this.data.checkout;
+    // do some other data handling?
+    if(typeof(option) !== "undefined") {
+      data.option = option;
+    }
+    return data;
+  }
+
+  /**
+   *
+   */
+  EETracker.prototype.sendTransaction = function() {
+    this.sendProductDetail();
+    this.ga('ee.ec:setAction', 'purchase', this.data.transaction);
+    this.ga('ee.send', 'pageview');
+  };
+
+  /**
+   *
+   */
+  EETracker.prototype.attachListener = function(selector, action) {
+    var els = document.querySelectorAll(selector);
+    Array.from(els).forEach(function(el) {
+      el.addEventListener('click', this.onClick.bind(this, action));
+    }, this);
+  };
+
+  /**
+   *
+   */
+  EETracker.prototype.onClick = function(action, event) {
+    var href = event.target.href || null;
+    var hash = event.target.dataset.hash || null;
+    var option = event.target.dataset.option || null;
+
+    var callback = noop;
+    if (href) {
+      callback = function() {
+        document.location = href;
+      };
+    }
+
+    switch(action) {
+      case 'addToCart':
+        this.addToCart(hash, callback);
+        break;
+      case 'removeFromCart':
+        this.removeFromCart(hash, callback);
+        break;
+      case 'checkout':
+        this.checkout(option, callback);
+      default:
+        break;
+    }
   };
 
   return EETracker;
 })();
+
+/**
+ * Private function to load GA if not present
+ */
+var loadGA = function() {
+  (function(i,s,o,g,r,a,m){
+    i['GoogleAnalyticsObject']=r;
+    i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)
+    }
+    i[r].l=1*new Date();
+    a=s.createElement(o);
+    m=s.getElementsByTagName(o)[0];
+    // a.async=1;
+    a.src=g;
+    m.parentNode.insertBefore(a,m)
+  })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+}
--- eecommerce/static/js/object_definitions.js
+++ eecommerce/static/js/object_definitions.js
@@ -47,8 +47,8 @@ var impressionObject = (function() {
    * @param {object} data
    */
   var impressionObject = function(data) {
-    this.id = data.id,
-    this.name = data.name,
+    this.id = data.id;
+    this.name = data.name;
 
     optionalFields.forEach(function(field) {
       if (data.hasOwnProperty(field)) {
@@ -60,22 +60,155 @@ var impressionObject = (function() {
   return impressionObject;
 })();
 
-var impressionFieldObject = (function() {
+/**
+ * Product data
+ *
+ * Value    Type      Req   Description
+ * id       String    Yes*  The product ID or SKU (e.g. P67890).
+ *                          *Either this field or name must be set.
+ * name     String    Yes*  The name of the product (e.g. Android T-Shirt).
+ *                          *Either this field or id must be set.
+ * brand    String    No    The brand associated with the product (e.g. Google).
+ * category String    No    The category to which the product belongs (e.g.
+ *                          Apparel). Use / as a delimiter to specify up to
+ *                          5-levels of hierarchy (e.g. Apparel/Men/T-Shirts).
+ * variant  String    No    The variant of the product (e.g. Black).
+ * price    Currency  No    The price of a product (e.g. 29.20).
+ * quantity Number    No    The quantity of a product (e.g. 2).
+ * coupon   String    No    The coupon code associated with a product (e.g. SUMMER_SALE13).
+ * position Number    No    The product's position in a list or collection (e.g. 2).
+ *
+ * Example:
+ *  {               // Provide product details in a productFieldObject.
+ *    'id': 'P12345',                   // Product ID (string).
+ *    'name': 'Android Warhol T-Shirt', // Product name (string).
+ *    'category': 'Apparel',            // Product category (string).
+ *    'brand': 'Google',                // Product brand (string).
+ *    'variant': 'Black',               // Product variant (string).
+ *    'position': 1,                    // Product position (number).
+ *    'dimension1': 'Member'            // Custom dimension (string).
+ *  });
+ */
+
+var productObject = (function() {
+  var optionalFields = [
+    'brand', 'category', 'variant', 'price', 'quantity', 'coupon', 'position'
+  ];
+
   /**
-   * a `impressionFieldObject` as defined by GA Enhanced Ecommerce docs
+   * a single productObject as defined by GA Enhanced Ecommerce docs
    * @constructor
-   * @param {array<object>} objects
-   * @param {string|undefined} currency
+   * @param {object} data
    */
-  var impressionFieldObject = function(objects, currency) {
-    this.impressions = objects.map(function(objectData) {
-      return new impressionObject(objectData);
-    });
+  var productObject = function(data) {
+    this.id = data.id;
+    this.name = data.name;
 
-    if (currency) {
-      this.currency = currency;
-    }
-  }
+    optionalFields.forEach(function(field) {
+      if (data.hasOwnProperty(field)) {
+        this[field] = data[field];
+      }
+    }, this);
+  };
 
-  return impressionFieldObject;
+  return productObject;
+})();
+
+/**
+ * Promotion Data
+ *
+ * Value    Type      Req   Description
+ * id       String    Yes*  The promotion ID (e.g. PROMO_1234).
+ *                          *Either this field or name must be set.
+ * name     String    Yes*  The name of the promotion (e.g. Summer Sale).
+ *                          *Either this field or id must be set.
+ * creative String    No    The creative associated with the promotion
+ *                          (e.g. summer_banner2).
+ * position String    No    The position of the creative (e.g. banner_slot_1).
+ *
+ * Example:
+ *
+ *   {
+ *
+ *   }
+ */
+var promoObject = (function() {
+  var optionalFields = ['creative', 'position'];
+
+  /**
+   * a single promoObject as defined by GA Enhanced Ecommerce docs
+   * @constructor
+   * @param {object} data
+   */
+  var promoObject = function(data) {
+    this.id = data.id;
+    this.name = data.name;
+
+    optionalFields.forEach(function(field) {
+      if (data.hasOwnProperty(field)) {
+        this[field] = data[field];
+      }
+    }, this);
+  };
+
+  return promoObject;
+})();
+
+/**
+ * Action Data
+ *
+ * Value        Type      Req   Description
+ * id           String    Yes*  The transaction ID (e.g. T1234).
+ *                              *Required if the action type is purchase/refund.
+ * affiliation  String    No    The store or affiliation from which this
+ *                              transaction occurred (e.g. Google Store).
+ * revenue      Currency  No    Specifies the total revenue or grand total
+ *                              associated with the transaction (e.g. 11.99).
+ *                              This value may include shipping, tax costs, or
+ *                              other adjustments to total revenue that you want
+ *                              to include as part of your revenue calculations.
+ *                              Note: if revenue is not set, its value will be
+ *                              automatically calculated using the product
+ *                              quantity and price fields of all products in the
+ *                              same hit.
+ * tax          Currency  No    The total tax associated with the transaction.
+ * shipping     Currency  No    The shipping cost associated with the
+ *                              transaction.
+ * coupon       String    No    The transaction coupon redeemed with the
+ *                              transaction.
+ * list         String    No    The list that the associated products belong
+ *                              to. Optional.
+ * step         Number    No    A number representing a step in the checkout
+ *                              process. Optional on checkout actions.
+ * option       String    No    Additional field for checkout and
+ *                              checkout_option actions that can describe option
+ *                              information on the checkout page, like selected
+ *                              payment method.
+ *
+ * Example:
+ *   {
+ *
+ *   }
+ */
+var actionObject = (function() {
+  var optionalFields = [
+    'affiliation', 'revenue', 'tax', 'shipping', 'coupon', 'list', 'step', 'option'
+  ];
+
+  /**
+   * a single actionObject as defined by GA Enhanced Ecommerce docs
+   * @constructor
+   * @param {object} data
+   */
+  var actionObject = function(data) {
+    this.id = data.id;
+
+    optionalFields.forEach(function(field) {
+      if (data.hasOwnProperty(field)) {
+        this[field] = data[field];
+      }
+    }, this);
+  };
+
+  return actionObject;
 })();
